43a1c1e307ad435836ead585358755d0acb3f3cd
Arthaudcom <arthaudmorin@gmail.com>
2022-03-23 15:42:18 -0400

    ajout du lien entre puppeteer et nodemailer #1

--- rtxBot.js
+++ rtxBot.js
@@ -2,60 +2,59 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const nodemailer = require("nodemailer");
 
+
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    const url = 'https://www.bestbuy.ca/fr-ca/produit/carte-graphique-avec-memoire-gddr6-de-8-go-geforce-rtx-3060-ti-de-nvidia/15166285';
+    //url du produit désiré
+    const url = 'https://www.bestbuy.ca/fr-ca/produit/energizer-chargeur-maxi-d-energizer-avec-4-piles-rechargeables-aa-nimh-chvcmwb4-chvcmwb4/10135654';
+    await page.waitForTimeout(3000); //Vérifie si le bouton de rupture de stock apparaît sur la page
+    await page.screenshot({path: 'test.png'});
 
     await page.goto(url);  //navigue vers l'url
-
+    //sélécteur css du bouton de rupture de stock, si il est sur la page cela signiie que le produit n'est pas disponible
     const dispo = '#test > button[disabled]';
+    //sélécteur css disponible lorsque la page est chargée
     const waiter = '#test > button';
-            await page.waitForSelector(waiter); //attend que le selecteur css se charge
-            try { //vérifie si la page existe bien
-                await page.waitForSelector(dispo, { timeout: 3000 })
-                if (await page.$(dispo) == null) {  //récupère le texte dans le selecteur de mail si il est bien présent
-                    
-                }
-            } catch (error) { }
-            await page.goBack();
-            await page.waitForSelector(Agences);
+    await page.waitForSelector(waiter); //attend que la page se charge
+    //await page.waitForSelector(dispo, { timeout: 3000 }); //Vérifie si le bouton de rupture de stock apparaît sur la page
+    if (await page.$(dispo) == null) {  //
+        mail().catch(console.error);
+    } else {
+         console.log("produit indisponible") }
     console.log('end');
     await browser.close();
 })();
 
 // async..await is not allowed in global scope, must use a wrapper
 async function mail() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtpbv.univ-lyon1.fr",
-    port: 587,
-    secure: false, // true for 465, false nfor other ports
-    auth: {
-      user: "p2001337", // generated ethereal user
-      pass: "@Assassin_sith64@", // generated ethereal password
-    },
-  });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtpbv.univ-lyon1.fr",
+        port: 587,
+        secure: false, // true for 465, false nfor other ports
+        auth: {
+            user: "p2001337", // generated ethereal user
+            pass: "@Assassin_sith64@", // generated ethereal password
+        },
+    });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Arthaud Morin" <arthaud.morin@etu.univ-lyon1.fr>', // sender address
-    to: "arthaudmorin@gmail.com", // list of receivers
-    subject: "!![AVAILABLE PRODUCT]!!", // Subject line
-    text: url, // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Arthaud Morin" <arthaud.morin@etu.univ-lyon1.fr>', // sender address
+        to: "arthaudmorin@gmail.com", // list of receivers
+        subject: "!![AVAILABLE PRODUCT]!!", // Subject line
+        text: "https://www.bestbuy.ca/fr-ca/produit/energizer-chargeur-maxi-d-energizer-avec-4-piles-rechargeables-aa-nimh-chvcmwb4-chvcmwb4/10135654", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
-main().catch(console.error);
+
